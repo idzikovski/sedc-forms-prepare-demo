e1e37da12f5b3c075b7329a38974a80035af141a
Ivan Djikovski <ivan.djikovski@wsa.com>
2021-10-26 14:21:31 +0200

    Added class-3 code

---
+++ RealEstate/RealEstate/Views/DetailsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RealEstate.Views
+{
+    public partial class DetailsPage : ContentPage
+    {
+        public DetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        void ChipView_ChipDeleted(object sender, EventArgs e)
+        {
+            flexLayout.Children.Remove((View)sender);
+        }
+    }
+}
