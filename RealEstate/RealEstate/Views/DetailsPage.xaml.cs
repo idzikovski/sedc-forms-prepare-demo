b63cee15733513ff85cccf54f15ea8fa3071bef4
Ivan Djikovski <ivan.djikovski@wsa.com>
2021-11-01 13:21:24 +0100

    Saved changes

--- RealEstate/RealEstate/Views/DetailsPage.xaml.cs
+++ RealEstate/RealEstate/Views/DetailsPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using RealEstate.Models;
-using RealEstate.ViewModels;
+﻿using RealEstate.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
